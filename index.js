4e3a04ff1862d99a99dee4dbb20d3b3e605b8181
Fernanda2209 <mafe02095@gmail.com>
2022-11-03 19:11:38 -0500

    Js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const displayValorAnterior = document.getElementById("valor-anterior");
-const displayValorActual = document.getElementById("valor-actual");
-const botonesNumeros = document.querySelectorAll(".numero");
-const botonesOperadores = document.querySelectorAll(".operador");
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
 
+const display = new Display(displayValorAnterior, displayValorActual);
 
-const calculadora = new Calculadora();
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
 
-console.log(calculadora.sumar(2,3))
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
